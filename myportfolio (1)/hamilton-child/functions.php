2e293f87a02415a714328927ca3ac487db14c0e4
cheylongandersen <47107397+cheylongandersen@users.noreply.github.com>
2019-08-22 23:53:17 +0200

    Add files via upload

---
+++ myportfolio (1)/hamilton-child/functions.php
@@ -0,0 +1,8 @@
+<?php
+ /* Connects the parent theme to the child theme */
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+
+function enqueue_parent_styles() {
+   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
+?>
